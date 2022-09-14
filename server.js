d98ad710e979d0196ec62a699c99b2cbc6681e00
revyrob <kayle.robson@gmail.com>
2022-09-13 23:28:25 -0700

    first commit

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const app = express();
-const PORT = 8080;
+require("dotenv").config();
+const PORT = process.env.PORT;
+
+const recyclingCenterRoute = require("./routes/api.recycling");
+const gameRoute = require("./routes/api.game");
+const educationRoute = require("./route/api.education");
+const buySellRoute = require("./route/api.buySell");
+
+app.use(cors());
+app.use(express.json());
+app.use("/recycling", recyclingCenterRoute);
+app.use("/game", gameRoute);
+app.use("/education", educationRoute);
+app.use("/buySell", buySellRoute);
 
 app.get("/", function (req, res) {
   res("Server is up");
