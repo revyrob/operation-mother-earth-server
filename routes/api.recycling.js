c23bdd125104e0106c9cfc706d1d303138a281c1
revyrob <kayle.robson@gmail.com>
2022-09-15 15:55:50 -0700

    backend map is working and the information from the google api

--- routes/api.recycling.js
+++ routes/api.recycling.js
@@ -15,23 +15,23 @@ let lon = "-123.11461581337777";
 // / function to get the data from the API
 let getMap = async () => {
   let response = await axios(
-    `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lon}&radius=5000&keyword=${input}&key=${GOOGLE_API}`
+    `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${input}%20in%20&key=${GOOGLE_API}&location=${lat},${lon}`
   );
   return response;
 };
 
-router.post("/test", (req, res) => {
-  console.log(req.body);
-  res.send(`You have entered these cordinates ${req.body}`);
-});
+// router.post("/test", (req, res) => {
+//   console.log(req.body);
+//   res.send(`You have entered these cordinates ${req.body}`);
+// });
 
 router.get("/", (req, res) => {
   let mapData = getMap();
   mapData.then((response) => {
-    console.log(response);
-    // res.json(response.data);
+    console.log(response.data.results);
+    const centers = response.data.results;
+    res.json(centers);
   });
-  // res.json(mapData);
 });
 
 module.exports = router;
