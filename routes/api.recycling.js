853555257e186c364fd021bd52e019caa0e7e6c1
revyrob <kayle.robson@gmail.com>
2022-09-14 19:21:00 -0700

    worked on backend for recycling map, works but doesn't search great

--- routes/api.recycling.js
+++ routes/api.recycling.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const axios = require("axios");
-// const recycling = require("./recycling");
 
 //create a useEffect and get the map to load after the DOM
 //no search handler
@@ -16,23 +15,23 @@ let lon = "-123.11461581337777";
 // / function to get the data from the API
 let getMap = async () => {
   let response = await axios(
-    `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lon}&radius=1500&keyword=${input}&key=${GOOGLE_API}`
+    `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lon}&radius=5000&keyword=${input}&key=${GOOGLE_API}`
   );
   return response;
 };
 
 router.post("/test", (req, res) => {
   console.log(req.body);
-  res.send("testing");
+  res.send(`You have entered these cordinates ${req.body}`);
 });
 
 router.get("/", (req, res) => {
   let mapData = getMap();
   mapData.then((response) => {
     console.log(response);
-    res.json(response.data);
+    // res.json(response.data);
   });
-  //   res.json(mapData);
+  // res.json(mapData);
 });
 
 module.exports = router;
