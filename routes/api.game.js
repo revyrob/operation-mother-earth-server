db034fdcefde7b46b35d15992f47649ccc22b993
revyrob <kayle.robson@gmail.com>
2022-09-17 22:57:46 -0700

    fixed json question file and connected created the game question router

--- routes/api.game.js
+++ routes/api.game.js
@@ -1,3 +1,25 @@
 const express = require("express");
 const router = express.Router();
 const fs = require("fs/promises");
+
+/*
+ *read game questions from json file
+ */
+function loadQuestionData(callback) {
+  fs.readFile("./data/gameQuestionsEnglish.json", "utf8", callback);
+}
+
+/*
+ *Get all the questions
+ */
+router.get("/", (req, res) => {
+  loadQuestionData((err, data) => {
+    if (err) {
+      res.send("error while getting question data");
+      //loop throug the data just taking the
+    } else {
+      const questions = JSON.parse(data);
+      res.json(videos);
+    }
+  });
+});
