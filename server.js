0faf88f3f99c4d5a9a167ba18db7cd094905be7d
revyrob <kayle.robson@gmail.com>
2022-09-18 14:44:36 -0700

    got the question server running

--- server.js
+++ server.js
@@ -4,22 +4,22 @@ require("dotenv").config();
 const PORT = process.env.PORT;
 const cors = require("cors");
 const recyclingCenterRoute = require("./routes/api.recycling");
-// const gameRoute = require("./routes/api.game");
+const gameQuestionsRoute = require("./routes/api.gameQuestions");
 // const educationRoute = require("./route/api.education");
 // const buySellRoute = require("./route/api.buySell");
 
 app.use(cors());
 app.use(express.json());
 app.use("/recycling", recyclingCenterRoute);
-app.use("/game", gameRoute);
+app.use("/gameQuestions", gameQuestionsRoute);
 // app.use("/education", educationRoute);
 // app.use("/buySell", buySellRoute);
 
 // const middleware = require("../src/routes");
 
-app.get("/", function (req, res) {
-  res("Server is up🚀");
-});
+// app.get("/", function (req, res) {
+//   res("Server is up🚀");
+// });
 
 app.listen(PORT, function () {
   console.log("Hello our server is up and running");
