c6e00f732db9991727a61842cca5ffd6ea6b1d59
revyrob <kayle.robson@gmail.com>
2022-09-27 16:00:45 -0700

    all databases and collections are connected.  need to test

--- routes/api.recycling.js
+++ routes/api.recycling.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const axios = require("axios");
 const fs = require("fs");
-// const Center = require("../models/SchemaCenter.js");
+const Center = require("../models/SchemaCenters.js");
 
 //create a useEffect and get the map to load after the DOM
 //no search handler
@@ -46,13 +46,13 @@ let getMap = async (lat, lng) => {
 };
 
 /*
- *get data from google api
+ *get data from google api - I don't need to use MongoDb since the data I recieve is
+ *dynamic
  */
 router.get("/", (req, res) => {
   let mapData = getMap(req.params.lat, req.params.lng);
   mapData
     .then((response) => {
-      console.log(response.data.results);
       const centers = response.data.results;
       res.json(centers);
     })
@@ -60,75 +60,100 @@ router.get("/", (req, res) => {
 });
 
 /*
- *Connection to MongoDB
+ *get data from written file
  */
-// router.get("/", async (req, res) => {
-//   try {
-//     let mapData = getMap(req.params.lat, req.params.lng);
-//     const center = await Center.find({});
-//     // console.log(question);
-//     res.send(center);
-//   } catch (err) {
-//     console.log(err);
-//     res.status(500).send({ error: err });
-//   }
+// router.get("/new", (req, res) => {
+//   loadCentersData((err, data) => {
+//     if (err) {
+//       res.send("error getting data from json file");
+//     } else {
+//       const centers = JSON.parse(data);
+//       res.json(centers);
+//     }
+//   });
 // });
 
 /*
- *get data from written file
+ *Connection to list of centers that users have entered in.  MongoDB
  */
-router.get("/new", (req, res) => {
-  loadCentersData((err, data) => {
-    if (err) {
-      res.send("error getting data from json file");
-    } else {
-      const centers = JSON.parse(data);
-      res.json(centers);
-    }
-  });
+router.get("/new", async (req, res) => {
+  try {
+    const center = await Center.find({});
+    console.log(center);
+    res.send(center);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ error: err });
+  }
+});
+
+/*
+ *posting to new e-waste recycling center to database. MongoDB
+ */
+router.post("/", (req, res) => {
+  //axois call to google geocoding
+  axios
+    .get(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${req.body.address}+${req.body.city}+${req.body.country}&key=${GOOGLE_API}`
+    )
+    .then((response) => {
+      lat = response.data.results[0].geometry.location.lat;
+      lng = response.data.results[0].geometry.location.lng;
+      //create a new center and push to array
+      let createCenter = {
+        name: req.body.business,
+        address: req.body.address,
+        city: req.body.city,
+        country: req.body.country,
+        lng: lng,
+        lat: lat,
+      };
+      console.log(createCenter);
+      new Center(createCenter).save();
+    });
 });
 
 //i need to get lat and long from front end
 //run getMap again and put lat and lng into api call
-router.post("/", (req, res) => {
-  loadCentersData((err, enteredData) => {
-    if (err) {
-      res.send("error getting data");
-    } else {
-      const centersDataParsed = JSON.parse(enteredData);
-      // console.log(req.body);
+// router.post("/", (req, res) => {
+//   loadCentersData((err, enteredData) => {
+//     if (err) {
+//       res.send("error getting data");
+//     } else {
+//       const centersDataParsed = JSON.parse(enteredData);
+//       // console.log(req.body);
 
-      //axois call to google geocoding
-      axios
-        .get(
-          `https://maps.googleapis.com/maps/api/geocode/json?address=${req.body.address}+${req.body.city}+${req.body.country}&key=${GOOGLE_API}`
-        )
-        .then((response) => {
-          lat = response.data.results[0].geometry.location.lat;
-          lng = response.data.results[0].geometry.location.lng;
-          //create a new center and push to array
-          const createCenter = {
-            name: req.body.business,
-            address: req.body.address,
-            city: req.body.city,
-            country: req.body.country,
-            lng: lng,
-            lat: lat,
-          };
-          //push the new center to the json
-          centersDataParsed.push(createCenter);
+//       //axois call to google geocoding
+//       axios
+//         .get(
+//           `https://maps.googleapis.com/maps/api/geocode/json?address=${req.body.address}+${req.body.city}+${req.body.country}&key=${GOOGLE_API}`
+//         )
+//         .then((response) => {
+//           lat = response.data.results[0].geometry.location.lat;
+//           lng = response.data.results[0].geometry.location.lng;
+//           //create a new center and push to array
+//           const createCenter = {
+//             name: req.body.business,
+//             address: req.body.address,
+//             city: req.body.city,
+//             country: req.body.country,
+//             lng: lng,
+//             lat: lat,
+//           };
+//           //push the new center to the json
+//           centersDataParsed.push(createCenter);
 
-          //save the stringified data to the json file
-          saveCentersData(JSON.stringify(centersDataParsed));
+//           //save the stringified data to the json file
+//           saveCentersData(JSON.stringify(centersDataParsed));
 
-          res.status(201).send("You have submitted info");
-        })
-        .catch((err) => console.log(err));
+//           res.status(201).send("You have submitted info");
+//         })
+//         .catch((err) => console.log(err));
 
-      //take information out for lat and long
-      //write to create center
-    }
-  });
-});
+//       //take information out for lat and long
+//       //write to create center
+//     }
+//   });
+// });
 
 module.exports = router;
