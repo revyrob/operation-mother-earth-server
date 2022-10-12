71ad7c2412e6597d702d5c8d2da92dc403d10626
revyrob <kayle.robson@gmail.com>
2022-10-11 22:40:11 -0700

    changed the radius to 20km

--- routes/api.recycling.js
+++ routes/api.recycling.js
@@ -41,7 +41,7 @@ let getMap = async (lat, lng) => {
 
   let response = await axios(
     // `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${input}&location=${lat}%2C${lng}&key=${GOOGLE_API}`
-    `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat}%2C${lng}&radius=25000&keyword=${input}&key=${GOOGLE_API}`
+    `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat}%2C${lng}&radius=20000&keyword=${input}&key=${GOOGLE_API}`
   );
   return response;
 };
